ea314a1e817cb6fda708761c7340f069bdf8a3d3
Letimba <142944577+Letimba@users.noreply.github.com>
2024-02-28 02:43:26 +0100

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,11 @@
-// List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+  'Notion Templates',
+  'Productivity',
+  'Personal Growth',
+  'Business',
+  'Student Life',
+  'Freelancing',
+  'Finance Tracking',
+  'Writing',
+  'Marketing'
 ] as const
